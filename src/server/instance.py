cb36d8aad3684b6796fbf1b642ed4df53fc41485
José Victor <victorlimaf55@gmail.com>
2024-03-13 16:46:47 -0300

    detalhes de padronização

--- src/server/instance.py
+++ src/server/instance.py
@@ -2,21 +2,22 @@ from flask import Flask
 from flask_restx import Api
 
 class Server():
-    def __init__(self, ):
+    def __init__(self):
         self.app = Flask(__name__)
-        self.api = Api(self.app,
-            title='dataWashes API',
+        self.api = Api(
+            self.app,
+            title='dataWASHES API',
             description='An API to request data on what the WASHES community discussed about software engineering economics in the last few years.',
             doc='/'
         )
 
-    def run(self, ):
+    def run(self):
         self.app.run( debug=True )
 
-    def getApi(self, ):
+    def getApi(self):
         return self.api
     
-    def getApp(self, ):
+    def getApp(self):
         return self.app
 
 server = Server()
