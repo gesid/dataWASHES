ca00cb3cef243b4b651d62e8c2af7e9ed6cfe693
Isaac Farias <isaacfarias006@gmail.com>
2024-03-25 09:49:46 -0300

    trying something else

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
-from src.server import server
 from src.controllers import authorsNs, editionsNs, papersNs
+from flask import Flask
+from flask_restx import Api
+
+class Server():
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.api = Api(
+            self.app,
+            title='dataWASHES API',
+            description='An API to request data on what the WASHES community discussed about software engineering economics in the last few years.',
+            doc='/'
+        )
+
+    def run(self):
+        self.app.run()
+
+    def getApi(self):
+        return self.api
     
+    def getApp(self):
+        return self.app
+
+server = Server()
+
 server.api.add_namespace(editionsNs)
 server.api.add_namespace(papersNs)
 server.api.add_namespace(authorsNs)
