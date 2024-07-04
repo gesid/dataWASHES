c96b688825d987bf179df1cecbfa47d780c34eb3
Jpereira29 <jorgepereira29ele@gmail.com>
2024-07-04 19:18:35 -0300

    chore: Refactor exemplo.py to separate routes for authors, papers, and editions

--- src/controllers/exemplo.py
+++ src/controllers/exemplo.py
@@ -3,8 +3,20 @@ from resouces.database_conn import DatabaseConn
 
 ns = Namespace(name='Exemplo', path='/exemplo')
 
-@ns.route("/")
+@ns.route("/authors")
 class Exemplo(Resource):
     def get(self):
         results = DatabaseConn.command("SELECT * FROM Authors")
+        return results
+    
+@ns.route("/papers")
+class Exemplo(Resource):
+    def get(self):
+        results = DatabaseConn.command("SELECT * FROM Papers")
+        return results
+    
+@ns.route("/editions")
+class Exemplo(Resource):
+    def get(self):
+        results = DatabaseConn.command("SELECT * FROM Editions")
         return results
