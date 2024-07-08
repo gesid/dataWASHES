360cefff41280f7b50bfd93fff9ffee485a171f9
José Victor <victorlimaf55@gmail.com>
2024-07-08 14:59:54 -0300

    Colocando o frontend da API como a rota principal

--- src/server/instance.py
+++ src/server/instance.py
@@ -1,14 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restx import Api
 
 class Server():
     def __init__(self):
-        self.app = Flask(__name__, template_folder='../templates', static_folder='../static')
+        self.app = Flask(
+            __name__,
+             template_folder='../templates',
+            static_folder='../static',
+        )
+
+        @self.app.route('/')
+        def swagger_template():
+            return render_template('swagger_ui.html')
+
         self.api = Api(
             self.app,
             title='dataWASHES API',
             description='dataWASHES is an open source Application Programming Interface (API) that aims to facilitate streamlined programmatic access to the Workshop on Social, Human, and Economic Aspects of Software (WASHES) proceedings.',
-            doc='/'
+            doc='/doc/',
         )
 
     def run(self):
