f264eb18c987fcf872b36b02cb233a313255c96d
Diogo Gabriel <145854574+DiogoG-dev@users.noreply.github.com>
2024-07-21 20:53:00 -0300

    Update editions.py
    Implementando a função de logging, do middleware "logging_washes.py", ao script "editions.py".

--- src/controllers/editions.py
+++ src/controllers/editions.py
@@ -2,6 +2,8 @@ from flask_restx import Resource, Namespace
 from flask import jsonify
 from resouces import editions_db, papers_db
 from models import edition, paper
+from flask import request
+from utils.logging_washes import log_request
 
 ns = Namespace(name='Editions', path='/editions')
 
@@ -14,6 +16,7 @@ class EditionsList(Resource):
         '''
     )
     def get(self):
+        log_request(request.method, request.path, 200)
         return editions_db
 
 @ns.route("/<int:id>")
@@ -31,7 +34,9 @@ class EditionById(Resource):
     def get(self, id):
         edition = next((e for e in editions_db if e["Edition_id"] == id), None)
         if edition is None:
+            log_request(request.method, request.path, 404)
             return jsonify({"error": "Edition not found"}), 404
+        log_request(request.method, request.path, 200)
         return edition
 
 @ns.route("/by-year/<int:year>")
@@ -47,16 +52,20 @@ class SearchEditions(Resource):
              })
     def get(self, year):
         if year is None:
+            log_request(request.method, request.path, 404)
             return jsonify({"error": "Missing 'year' parameter"}), 404
 
         # Adicionando condicional somente para os anos com editions existentes
         if not (2016 <= year <= 2023):
+            log_request(request.method, request.path, 404)
             return jsonify({"error": "Year not within range (2016-2023)"}), 404
 
         matched_editions = [e for e in editions_db if e["Year"] == int(year)]
         if matched_editions:
+            log_request(request.method, request.path, 200)
             return matched_editions, 200
         else:
+            log_request(request.method, request.path, 404)
             return jsonify({"error": "Edition not found for year {}".format(year)}), 404
 
 @ns.route("/<int:id>/papers")
@@ -74,8 +83,10 @@ class PapersByEdition(Resource):
     def get(self, id):
         edition = next((e for e in editions_db if e["Edition_id"] == id), None)
         if edition is None:
+            log_request(request.method, request.path, 404)
             return jsonify({"error": "Edition not found"}), 404
         papers_in_edition = [
             paper for paper in papers_db if paper["Paper_id"] in edition["Papers"]
         ]
+        log_request(request.method, request.path, 200)
         return papers_in_edition
