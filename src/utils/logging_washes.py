ed44bbe595f7d6f7c59495fff7b51a27cba72cab
Diogo Gabriel <145854574+DiogoG-dev@users.noreply.github.com>
2024-07-24 17:08:43 -0300

    Update logging_washes.py
    Adicionando novas funções para a geração de logs, exclusivas para "papers", com o objetivo de entregar uma visualização das rotas, tendo em vista que a função principal não se comporta adequadamente.

--- src/utils/logging_washes.py
+++ src/utils/logging_washes.py
@@ -15,3 +15,26 @@ def log_request(method, route, status):
         "status": status
     }
     logging.info(log_details)
+
+# Excluvivas para "papers":
+def log_request_papers_error(method, route, path, argument, status):
+    route = f"{route}{path}/{argument}"
+    log_details = {
+        "ip": request.remote_addr,
+        "time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "method": method,
+        "route": route,
+        "status": status
+    }
+    logging.info(log_details)
+
+def log_request_papers_success(method, route, status, all_paths_and_arguments):
+    route = f"{route}{all_paths_and_arguments}"
+    log_details = {
+        "ip": request.remote_addr,
+        "time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "method": method,
+        "route": route,
+        "status": status
+    }
+    logging.info(log_details)
