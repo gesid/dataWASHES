691016a4e6b8ce84174462dfb027939b7c18840b
José Victor <victorlimaf55@gmail.com>
2024-07-30 19:52:47 -0300

    Renomeando utils para api_utils para evitar conflito

--- src/app.py
+++ src/app.py
@@ -1,9 +1,8 @@
-from controllers import authorsNs, editionsNs, papersNs, exemploNs
+from controllers import authorsNs, editionsNs, papersNs
 from server import server
 
 server.api.add_namespace(editionsNs)
 server.api.add_namespace(papersNs)
 server.api.add_namespace(authorsNs)
-#server.api.add_namespace(exemploNs)
 
 server.run()
