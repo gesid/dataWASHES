94b86a5538f0a49244cf57a7ed1c5df2db8f4751
José Victor <victorlimaf55@gmail.com>
2024-08-01 13:43:40 -0300

    Corrigindo falha no teste test_get_papers_with_invalid_year_parameter

--- src/controllers/papers.py
+++ src/controllers/papers.py
@@ -113,7 +113,11 @@ class GetPapersByYear(Resource):
 
         if not year:
             log_request(request.method, request.path, 400)
-            ns.abort(404, message="Missing 'year' parameter")
+            ns.abort(400, message="Missing 'year' parameter")
+
+        if not str(year).isnumeric():
+            log_request(request.method, request.path, 400)
+            ns.abort(400, message="Invalid 'year' parameter")
 
         papers.filter_by({"Year": year})
         if papers.is_empty():
