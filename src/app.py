28359bd7fd8f807711665a335bbdead707a9814e
José Victor <victorlimaf55@gmail.com>
2024-08-14 15:11:27 -0300

    Melhorando a forma de acesso aos arquivos JSON

--- src/app.py
+++ src/app.py
@@ -3,12 +3,14 @@ from controllers import authorsNs, editionsNs, papersNs
 from server import server
 from api_utils import convert_to_csv
 
+
 @server.api.representation('text/csv')
 def data_csv(data, code, headers):
-    '''Get result in csv '''
+    """Get result in csv"""
     resp = make_response(convert_to_csv(data), code)
     resp.headers.extend(headers)
     return resp
+
 
 server.api.add_namespace(editionsNs)
 server.api.add_namespace(papersNs)
