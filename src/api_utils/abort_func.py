e19d589d9e27d4b2bac9c2c93ff1ffd00ba0c5f0
José Victor <victorlimaf55@gmail.com>
2024-08-14 15:35:15 -0300

    Update abort_func.py

--- src/api_utils/abort_func.py
+++ src/api_utils/abort_func.py
@@ -1,5 +1,4 @@
 from flask_restx import Namespace # type: ignore
-from flask import request
 from .logging_washes import log_request
 
 def abort_execution(ns: Namespace, message: str, error_code: int) -> None:
@@ -7,5 +6,5 @@ def abort_execution(ns: Namespace, message: str, error_code: int) -> None:
     Calls the function that properly abort the current request\n
     and creates the log for the request.
     """
-    log_request(request.method, request.path, error_code)
+    log_request(error_code)
     ns.abort(error_code, message=message, error_code=error_code)
